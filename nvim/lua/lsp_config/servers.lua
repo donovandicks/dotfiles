3e4799d0ef75bf78e1d50a4daaf9ba2ef258376f
Donovan Dicks <donovan.dicks@outlook.com>
2022-07-08 12:27:26 -0400

    Refactor, fix pairs for coq, remove some ts

--- nvim/lua/lsp_config/servers.lua
+++ nvim/lua/lsp_config/servers.lua
@@ -1,23 +1,53 @@
-local success, lspconfig = pcall(require, 'lspconfig')
-if not success then
-  return
+function load_plugin(plugin_name)
+  local success, plugin = pcall(require, plugin_name)
+  if not success then
+    error()
+  end
+
+  return plugin
 end
 
-local success, coq = pcall(require, 'coq')
-if not success then
-  return
-end
+local lspconfig = load_plugin('lspconfig')
+local coq = load_plugin('coq')
 
-_G.lsp_organize_imports = function()
-  local params = {
-    command = '_typescript.organizeImports',
-    arguments = { vim.api.nvim_buf_get_name(0) },
-    title = '',
-  }
-  vim.lsp.buf.execute_command(params)
-end
+-- Auto Indent Pairs
+local remap = vim.api.nvim_set_keymap
+local npairs = require('nvim-autopairs')
 
-vim.cmd('command! LspOrganize lua lsp_organize_imports()')
+npairs.setup({ map_bs = false, map_cr = false })
+
+vim.g.coq_settings = { keymap = { recommended = false } }
+
+-- these mappings are coq recommended mappings unrelated to nvim-autopairs
+remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+
+-- skip it, if you use another global object
+_G.MUtils= {}
+
+MUtils.CR = function()
+  if vim.fn.pumvisible() ~= 0 then
+    if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+      return npairs.esc('<c-y>')
+    else
+      return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+    end
+  else
+    return npairs.autopairs_cr()
+  end
+end
+remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+
+MUtils.BS = function()
+  if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+    return npairs.esc('<c-e>') .. npairs.autopairs_bs()
+  else
+    return npairs.autopairs_bs()
+  end
+end
+remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
 
 -- Global Diagnostic Mappings
 local opts = { noremap=true, silent=true }
@@ -61,6 +91,11 @@ local servers = {
   'jsonls',
   'terraformls',
   'cmake',
+  'denols',
+}
+
+vim.g.markdown_fenced_languages = {
+  "ts=typescript"
 }
 
 -- Use a loop to conveniently call 'setup' on multiple servers and map buffer local
@@ -86,62 +121,51 @@ for _, lsp in pairs(servers) do
   }))
 end
 
---   lspconfig.diagnosticls.setup {
---     on_attach = on_attach,
---     filetypes = vim.tbl_keys(filetypes),
---     init_options = {
---       filetypes = filetypes,
---       linters = linters,
---       formatters = formatters,
---       formatFiletypes = formatFiletypes,
---     }
---   }
-
 -- ECMAScript
 
-local eslint = {
-  lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
-  lintIgnoreExitCode = true,
-  lintStdin = true,
-  lintFormats = { '%f:%l:%c: %m' },
-  formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
-  formatStdin = true,
-}
+-- local eslint = {
+--   lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
+--   lintIgnoreExitCode = true,
+--   lintStdin = true,
+--   lintFormats = { '%f:%l:%c: %m' },
+--   formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
+--   formatStdin = true,
+-- }
 
-local prettier = {
-  formatCommand = 'prettier --stdin-filepath ${INPUT}',
-  formatStdin = true,
-}
+-- local prettier = {
+--   formatCommand = 'prettier --stdin-filepath ${INPUT}',
+--   formatStdin = true,
+-- }
 
-local linters = {
-  eslint = {
-    sourceName = 'eslint',
-    command = 'eslint_d',
-    rootPatterns = {'.eslintrc.js', 'package.json' },
-    debounce = 100,
-    args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
-    parseJson = {
-      errorsRoot = '[0].messages',
-      line = 'line',
-      column = 'column',
-      endLine = 'endLine',
-      endColumn = 'endColumn',
-      message = '${message} [${ruleId}]',
-      security = 'severity',
-    },
-    securities = { [2] = 'error', [1] = 'warning', },
-  }
-}
+-- local linters = {
+--   eslint = {
+--     sourceName = 'eslint',
+--     command = 'eslint_d',
+--     rootPatterns = {'.eslintrc.js', 'package.json' },
+--     debounce = 100,
+--     args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
+--     parseJson = {
+--       errorsRoot = '[0].messages',
+--       line = 'line',
+--       column = 'column',
+--       endLine = 'endLine',
+--       endColumn = 'endColumn',
+--       message = '${message} [${ruleId}]',
+--       security = 'severity',
+--     },
+--     securities = { [2] = 'error', [1] = 'warning', },
+--   }
+-- }
 
-local formatters = {
-  prettier = { command = 'prettier', args = { '--stdin-filepath', '%filepath' }}
-}
+-- local formatters = {
+--   prettier = { command = 'prettier', args = { '--stdin-filepath', '%filepath' }}
+-- }
 
-local formatFiletypes = { typescript = 'prettier', }
+-- local formatFiletypes = { typescript = 'prettier', }
 
-lspconfig.tsserver.setup {
-  on_attach = function(client)
-    client.resolved_capabilities.document_formatting = false
-    on_attach(client)
-  end
-}
+-- lspconfig.tsserver.setup {
+--   on_attach = function(client)
+--     client.resolved_capabilities.document_formatting = false
+--     on_attach(client)
+--   end
+-- }
